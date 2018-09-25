6f1f2eadcc25ee9b69a3aef76297dbea204fa971
Alfonso Sánchez Uzábal <skotperez@voragine.net>
2018-09-25 13:53:44 +0200

    Workshops list associated to sgs-home page template

--- includes/pagetemplater/sgs-home.php
+++ includes/pagetemplater/sgs-home.php
@@ -1,7 +1,7 @@
 <?php
 /*
- * Template Name: Good to Be Bad
- * Description: A Page Template with a darker design.
+ * Template Name: Ressources home page
+ * Description: Home page for activezvosressources.tools
  */
 
-echo '<iframe width="1280" height="720" src="//www.youtube.com/embed/2Bls1KKDwmo" frameborder="0" allowfullscreen></iframe>';
+get_template_part( 'page' );
--- sagesses-ressources.php
+++ sagesses-ressources.php
@@ -16,6 +16,7 @@ $workshop_pt = 'atelier';
 
 
 // PAGE TEMPLATES CREATOR
+// to add more templates edit pagetemplater, around line 72
 include("includes/pagetemplater/pagetemplater.php");
 
 // LOAD PLUGIN TEXT DOMAIN
@@ -63,7 +64,7 @@ add_filter('the_content','sgs_ressources_atelier_add_form');
 function sgs_ressources_atelier_add_form($content) {
 	global $post;
 	global $workshop_pt;
-	if ( get_post_type($post) == $workshop_pt ) {
+	if ( get_post_type($post) == $workshop_pt && is_single() ) {
 		$content .= gravity_form( '1', true, true, false, null, false, '', false );
 	}
 	return $content;
@@ -80,5 +81,78 @@ function sgs_ressources_atelier_inscrits_presents_update($entry, $form) {
 	$nf[] = $entry['1'];
 	update_post_meta($p->ID, '_atelier_inscrits_presents', $nf );
 
+}
+
+// PRODUCE ATELIERS LIST
+// to include it in home page
+add_filter('the_content','sgs_ressources_atelier_list');
+function sgs_ressources_atelier_list($content) {
+
+	if ( !is_page_template('sgs-home.php') ) return $content;
+
+	global $workshop_pt;
+	$args = array(
+		'post_type' => $workshop_pt,
+		'posts_per_page' => -1,
+		'orderby' => 'meta_value',
+		'meta_key' => '_atelier_date'
+	);
+	$ateliers = get_posts($args);
+	$today = date('Y-m-d');
+	$a_rows = '';
+	$ap_rows = '';
+	foreach ( $ateliers as $a ) {
+		$a_perma = get_permalink($a->ID);
+		$a_date = get_post_meta($a->ID,'_atelier_date',true);
+		$a_time = get_post_meta($a->ID,'_atelier_heure',true);
+		$a_registered = get_post_meta($a->ID,'_atelier_inscrits',false);
+		$ar_count = ( $a_registered[0] === FALSE ) ? 0 : count($a_registered);
+		$a_present = get_post_meta($a->ID,'_atelier_inscrits_presents',false);
+		$ap_count = ( $a_present[0] === FALSE ) ? 0 : count($a_present);
+		if ( $today <= $a_date ) {
+			$a_rows .= '
+			<tr>
+				<td><a href="'.$a_perma.'">'.$a->post_title.'</a></td>
+				<td>'.$a_date.'</td>
+				<td>'.$a_time.'</td>
+				<td>'.$ar_count.'</td>
+				<td>'.$ap_count.'</td>
+			</tr>
+			';
+		} else {
+			$ap_rows .= '
+			<tr>
+				<td><a href="'.$a_perma.'">'.$a->post_title.'</a></td>
+				<td>'.$a_date.'</td>
+				<td>'.$a_time.'</td>
+				<td>'.$ar_count.'</td>
+				<td>'.$ap_count.'</td>
+			</tr>
+			';
+		}
+	}
+	$a_head = '
+		<thead><tr>
+			<th>'.__("Workshop","sgs_ressources").'</th>
+			<th>'.__("Date","sgs_ressources").'</th>
+			<th>'.__("Time","sgs_ressources").'</th>
+			<th>'.__("Registered people","sgs_ressources").'</th>
+			<th>'.__("Present people","sgs_ressources").'</th>
+		</tr></thead>
+	';
+	$a_table = ( $a_rows != "" ) ? '
+	<table>
+		'.$a_head.'
+		<tbody>'.$a_rows.'</tbody></table>
+	' : '';
+	$ap_table = ( $ap_rows != "" ) ? '
+	<h2>'.__('Past workshops','sgs_ressources').'</h2>
+	<table>
+		'.$a_head.'
+		<tbody>'.$a_rows.'</tbody></table>
+	' : '';
+
+	$content .= $a_table.$ap_table;
+	return $content;
 }
 ?>
